6e88665f6ab54380919929c7c87160cde0f5a5d7
17llarmour <17llovelandarmour@uobschool.org.uk>
2022-12-26 17:48:50 +0000

    beginnings of web handling for frontend started

--- FrontEnd/main.py
+++ FrontEnd/main.py
@@ -4,6 +4,10 @@ import json
 from random import *
 from pygame import *
 
+
+# https://www.sitepoint.com/python-multiprocessing-parallel-programming/#:~:text=One%20way%20to%20achieve%20parallelism,multiprocessing%20accomplishes%20process%2Dbased%20parallelism.
+# Potentially use that to always update cannon position, have it run in parallel with the rest of the code
+
 def newGame():
     url = "http://localhost/reset" + "?reset=" + "yes"
     r = requests.post(url)
@@ -11,6 +15,11 @@ def newGame():
     r = requests.post(url)
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def getStates(pos):
+    shoot = requests.post(url="http://localhost/state?place=yes")
+    grid = requests.get("http://localhost/state")
+    shooty = requests.get("http://localhost/shootyState")
 
+
+if __name__ == '__main__':
+    pass
